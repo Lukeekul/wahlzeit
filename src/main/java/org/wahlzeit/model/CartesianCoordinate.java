97d8b5843bfdfead576677d5b7cbf0615e6bff8b
Luke <lukeekul@gmx.de>
2018-11-19 09:11:25 +0100

    [Coordinate] beautify classes

--- src/main/java/org/wahlzeit/model/CartesianCoordinate.java
+++ src/main/java/org/wahlzeit/model/CartesianCoordinate.java
@@ -10,7 +10,10 @@ public class CartesianCoordinate implements Coordinate{
 	private double m_y;
 	private double m_z;
 	private final double ETTA = 10e-7;
-	
+
+	/*
+	 *
+	 */
 	public CartesianCoordinate(double x, double y, double z) {
 		assert x >= 0.0;
 		assert y >= 0.0;
@@ -20,14 +23,19 @@ public class CartesianCoordinate implements Coordinate{
 		m_z = z;
 	}
 
+	/*
+	 *
+	 */
 	public CartesianCoordinate(CartesianCoordinate point) {
 		m_x = point.m_x;
 		m_y = point.m_y;
 		m_z = point.m_z;
 	}
-	
+
+	/*
+	 *
+	 */
 	public double getCartesianDistance(CartesianCoordinate point) {
-		
 		double distanceX = Math.pow(point.m_x - m_x, 2);
 		double distanceY = Math.pow(point.m_y - m_y, 2);
 		double distanceZ = Math.pow(point.m_z - m_z, 2);
@@ -35,13 +43,18 @@ public class CartesianCoordinate implements Coordinate{
 		return Math.sqrt(distanceX + distanceY + distanceZ);
 	}
 
+	/*
+	 *
+	 */
 	public double getCartesianDistance(SphericCoordinate point) {
 		CartesianCoordinate tmp = point.asCartesianCoordinate();
 		return getCartesianDistance(tmp);
 	}
-	
-	public boolean isAlmostEqual(CartesianCoordinate point, double etta) {
 
+	/*
+	 *
+	 */
+	public boolean isAlmostEqual(CartesianCoordinate point, double etta) {
 		double diff_x = Math.abs(m_x - point.m_x);
 		double diff_y = Math.abs(m_y - point.m_y);
 		double diff_z = Math.abs(m_z - point.m_z);
@@ -53,30 +66,47 @@ public class CartesianCoordinate implements Coordinate{
 		return false;
 	}
 
+	/*
+	 *
+	 */
 	public boolean isEqual(CartesianCoordinate point) {
 		return isAlmostEqual(point, ETTA);
 	}
-	
+
+	/*
+	 *
+	 */
 	public boolean equals(CartesianCoordinate point) {
 		return isEqual(point);
 	}
 
+	/*
+	 *
+	 */
 	public boolean isEqual(SphericCoordinate point) {
 		return isEqual(point.asCartesianCoordinate());
 	}
 
+	/*
+	 *
+	 */
 	public double getCentralAngle(CartesianCoordinate point) {
 		SphericCoordinate tmp_this = this.asSphericCoordinate();
 		SphericCoordinate tmp_other = point.asSphericCoordinate();
 		return tmp_this.getCentralAngle(tmp_other);
-
 	}
 
+	/*
+	 *
+	 */
 	public double getCentralAngle(SphericCoordinate point) {
 		SphericCoordinate tmp = this.asSphericCoordinate();
 		return tmp.getCentralAngle(point);
 	}
 
+	/*
+	 *
+	 */
 	public SphericCoordinate asSphericCoordinate() {
 		double radius = Math.sqrt(Math.pow(m_x, 2) + Math.pow(m_y, 2) + Math.pow(m_z, 2));
 		double theta = Math.acos(m_z/radius);
@@ -84,6 +114,9 @@ public class CartesianCoordinate implements Coordinate{
 		return new SphericCoordinate(Math.toDegrees(phi), Math.toDegrees(theta), radius);
 	}
 
+	/*
+	 *
+	 */
 	public CartesianCoordinate asCartesianCoordinate() {
 		return this;
 	}
--- src/main/java/org/wahlzeit/model/SphericCoordinate.java
+++ src/main/java/org/wahlzeit/model/SphericCoordinate.java
@@ -11,21 +11,30 @@ public class SphericCoordinate implements Coordinate{
 	private double m_radius;
 	private final double ETTA = 10e-7;
 
+	/*
+	 *
+	 */
 	public SphericCoordinate(double phi_degree, double theta_degree, double radius) {
 		assert radius >= 0.0;
 		assert phi_degree >= 0.0 && phi_degree < 360.0;
-		// assert theta_degree >= 0.0 && theta_degree < 180.0;
+		assert theta_degree >= 0.0 && theta_degree < 180.0;
 		m_phi =  Math.toRadians(phi_degree);
 		m_theta = Math.toRadians(theta_degree);
 		m_radius = radius;
 	}
 
+	/*
+	 *
+	 */
 	public SphericCoordinate(SphericCoordinate point){
 		m_phi = point.m_phi;
 		m_theta = point.m_theta;
 		m_radius = point.m_radius;
 	}
 
+	/*
+	 *
+	 */
 	public CartesianCoordinate asCartesianCoordinate() {
 		double x = m_radius * Math.sin(m_theta) * Math.cos(m_phi);
 		double y = m_radius * Math.sin(m_theta) * Math.sin(m_phi);
@@ -33,18 +42,26 @@ public class SphericCoordinate implements Coordinate{
 		return new CartesianCoordinate(x, y, z);
 	}
 
+	/*
+	 *
+	 */
 	public SphericCoordinate asSphericCoordinate() {
 		return this;
 	}
 
+	/*
+	 *
+	 */
 	public double getCartesianDistance(SphericCoordinate point) {
 		CartesianCoordinate tmp_this = this.asCartesianCoordinate();
 		CartesianCoordinate tmp_other = point.asCartesianCoordinate();
 		return tmp_this.getCartesianDistance(tmp_other);
 	}
 
+	/*
+	 *
+	 */
 	public double getCentralAngle(SphericCoordinate point) {
-
 		double delta_x = Math.cos(point.m_phi) * Math.cos(point.m_theta) - Math.cos(m_phi) * Math.cos(m_theta);
 		double delta_y = Math.cos(point.m_phi) * Math.sin(point.m_theta) - Math.cos(m_phi) * Math.sin(m_theta);
 		double delta_z = Math.sin(point.m_phi) - Math.sin(m_phi);
@@ -55,25 +72,40 @@ public class SphericCoordinate implements Coordinate{
 		return Math.toDegrees(centralAngle);
 	}
 
+	/*
+	 *
+	 */
 	public double getCartesianDistance(CartesianCoordinate point) {
 		CartesianCoordinate tmp = this.asCartesianCoordinate();
 		return  tmp.getCartesianDistance(point);
 	}
 
+	/*
+	 *
+	 */
 	public double getCentralAngle(CartesianCoordinate point) {
 		SphericCoordinate tmp = point.asSphericCoordinate();
 		return getCentralAngle(tmp);
 	}
 
+	/*
+	 *
+	 */
 	public boolean isEqual(CartesianCoordinate point) {
 		SphericCoordinate tmp = point.asSphericCoordinate();
 		return isEqual(tmp);
 	}
 
+	/*
+	 *
+	 */
 	public boolean isEqual(SphericCoordinate point) {
 		return isAlmostEqual(point, ETTA);
 	}
 
+	/*
+	 *
+	 */
 	private boolean isAlmostEqual(SphericCoordinate point, double etta) {
 		double diff_phi = Math.abs(m_phi - point.m_phi);
 		double diff_theta = Math.abs(m_theta - point.m_theta);
@@ -82,7 +114,6 @@ public class SphericCoordinate implements Coordinate{
 		if (diff_phi < etta && diff_theta < etta  && diff_radius < etta){
 			return true;
 		}
-
 		return false;
 	}
 }
